be66bf6c0e1050e9f702bcca72dc067dd11d4ffe
Sayem Kalim <101326225+sayemkalim@users.noreply.github.com>
2026-08-31 18:27:12 +0530

    refactor: revert to environment variable for BACKEND_URL configuration

--- src/utils/url/index.js
+++ src/utils/url/index.js
@@ -1,2 +1,1 @@
-// export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-export const BACKEND_URL = "https://shagunbeautybe.onrender.com"
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
